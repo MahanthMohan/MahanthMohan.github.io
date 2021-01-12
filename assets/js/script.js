dc4bfe933086d0232826c25ff146698fd97776a3
Mahanth <mohan.mahanth@gmail.com>
2021-01-12 08:46:40 -0800

    succesfully made it responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,57 +7,58 @@ function getGithubStats() {
         .then((data) => { 
             data.map((v) => {
             languages.push(v["language"])
-        })
+        }
+    )
         
-        // get the unique elements of the array into a new array called categories
-        var categories = [...new Set(languages)].filter(v => v != null)
+    // get the unique elements of the array into a new array called categories
+    var categories = [...new Set(languages)].filter(v => v != null)
 
-        function elementCount(lst, e) {
-            var count = 0
-            for(var i = 0; i < lst.length; i++) {
-                if(lst[i] == e) {
-                    count += 1
-                } else {
-                    count += 0
-                }
+    function elementCount(lst, e) {
+        var count = 0
+        for(var i = 0; i < lst.length; i++) {
+            if(lst[i] == e) {
+                count += 1
+            } else {
+                count += 0
             }
-            return count
         }
+        return count
+    }
 
-        for(var k = 0; k < categories.length; k++) {
-            frequencies.push(elementCount(languages, categories[k]))
-        }
+    for(var k = 0; k < categories.length; k++) {
+        frequencies.push(elementCount(languages, categories[k]))
+    }
 
-        var ctx = document.getElementById("statsChart")
-        var myDoughnutChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: categories,
-                datasets: [{
-                    label: "Languages Used",
-                    backgroundColor: [                
-                        "#643cbc",
-                        "#bc6ccc",
-                        "#f4b464",
-                        "#f43c5c",
-                        "#fc0494",
-                        "#047cfc",
-                        "#66ff00"
-                    ],
-                    borderColor: "transparent",
-                    data: frequencies,
-                }
-                ]
+    var ctx = document.getElementById("statsChart")
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: categories,
+            datasets: [{
+                label: "Languages Used",
+                backgroundColor: [                
+                    "#643cbc",
+                    "#bc6ccc",
+                    "#f4b464",
+                    "#f43c5c",
+                    "#fc0494",
+                    "#047cfc",
+                    "#66ff00"
+                ],
+                borderColor: "transparent",
+                data: frequencies,
+            }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                fontSize: 22,
+                text: "Languages Used (# of Repos)",
             },
-            options: {
-                title: {
-                    display: true,
-                    fontSize: 22,
-                    text: "Languages Used (# of Repos)",
-                },
-                maintainAspectRatio: false,
-                responsive: false,
-            },
-        });
-    })
+            maintainAspectRatio: false,
+            responsive: true,
+        },
+    });
+})
 }
